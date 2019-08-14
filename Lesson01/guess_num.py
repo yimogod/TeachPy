2e193583cf5c7d00f1317fc3d93666098455e03b
zhibin.liu <89723453@qq.com>
2019-08-14 20:47:56 +0800

    store

--- Lesson01/guess_num.py
+++ Lesson01/guess_num.py
@@ -1,12 +1,12 @@
 target = 639
-print("输入一个数字猜一下我想的是什么数字. <1000以内>")
+print("输入一个数字猜一下我想的是什么数字.")
 
-src = int(input())
-while src != target:
-    if src < target :
+answer = int(input())
+while answer != target:
+    if answer < target:
         print("小了")
-    elif src > target:
+    elif answer > target:
         print("大了")
-    src = int(input())
+    answer = int(input())
 
 print("恭喜你, 真聪明. 答对了!")
--- Lesson02/square_spiral01.py
+++ Lesson02/square_spiral01.py
@@ -1,6 +1,15 @@
 import turtle
 
 p = turtle.Pen()
+turtle.bgcolor("black")
 for x in range(100):
+    #如果是偶数,用红笔
+    if x % 2 == 0 :
+        p.pencolor("red")
+    else:#否则用黑笔
+        p.pencolor("yellow")
+
     p.forward(x)
-    p.left(90)
+    #p.circle(x)
+    p.left(91)
+input()
